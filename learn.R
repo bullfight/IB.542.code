25a4ab62a41ca34f353623a3d3b586e48a1c8808
bullfight <p.schmitz@gmail.com>
2010-09-09 20:10:51 -0500

    Added loads of description and worked through errors in learn.R

--- learn.R
+++ learn.R
@@ -11,20 +11,33 @@
 # As you work through this, you should copy, select code chunks from this script and 
 # paste them into the console prompt
 
-# start by seting your working directory **rewrite to be appropriate to your computer**
-setwd("C:/Documents and Settings/Desktop/IB.542/")
-# setwd("~/Desktop/2010.IB.542/") #on MAC OS X
-# setwd("~/Dropbox/classes/2010.IB.542/code/2010.IB.542.Temperature/") # pat's directory
+# start by seting your working directory
 
 # In Windows, it may simpler to change the working directory from the menu bar, goto
 # File > Change dir...
 
+# On OS X you can do the same by going to 
+# Misc > Change Working Directory...
+# or hitting  ⌘ + D
+
+# You can also use the command setwd() as in
+# setwd("C:/Documents and Settings/Desktop/IB.542/") # On Windows
+# setwd("~/Desktop/2010.IB.542/") # on MAC OS X
+# setwd("~/Dropbox/classes/2010.IB.542/code/2010.IB.542.Temperature/") # Pat's directory
+
+# now check to make sure you are in the right directory
+getwd()
+# and
+list.files()
+
+# Notice that all text following "#" are comments, and will be ignored by the console interpreter
 
 
 # first clear the workspace, this is a good way to be sure
-# 	that you know exactly what you are working with
+# that you know exactly what you are working with
+
 rm(list=ls()) # clears all objects in workspace
-ls() 			# check to see that the workspace is empty
+ls() # check to see that the workspace is empty
 
 # Let's read in the data and put it into an object called dat
 dat <- read.csv(
@@ -34,23 +47,35 @@ dat <- read.csv(
 	as.is = TRUE
 )
 
-
 # R is an object-ariented programming language
-# 	this means that each variable is an "object"
-# 	that can be queried or manipulated
-#	dat is an arbitray name, I could just as easily
-#	have put the data in an object called hotdogz
+# this means that each variable is an "object"
+# that can be queried or manipulated
+# "dat" is an arbitray name, I could just as easily
+# have put the data in an object named "cookies"
+# It is important to note that R is case-sensitive
+# cookies ≠ Cookies ≠ COOKIES
 
-# read.csv is a function used to import csv files.
+# read.csv() is a function used to import csv files.
+# all functions in R follow the form of
+# function()
+# options for the function you are using are given
+# inside of the parenthesis, separated by commas "(option 1 = 1, option2 = 2)"
+
 # you can read about the function by typing
 ?read.csv
+# or see the basic inputs and sometimes source code by typing
+read.csv
 
 # you can access documentation about any function in R in this manner
-
+# The documentation is your friend, it is easy to access,
+# consistent in form, and there are almost always examples at the end 
+# of the documentation for any given function
+# It may however take a bit to get used to they layout
 
 
 
 # Description of Data ######################
+
 # This is a 10 minute data record from SoyFace 2010
 
 # DOY 	- Day of Year 
@@ -65,7 +90,8 @@ str(dat) 	# prints a list of the variables in the data.frame
 head(dat)	# prints first six rows
 
 # What if we want to just look at AirTemp
-#		Here are some methods that all do the same thing
+
+# Here are some methods that all do the same thing
 head(dat$Ta)
 dat$Ta[1:5]
 dat[1:5, "Ta"]
@@ -88,8 +114,7 @@ seq(2, 100, 2) 			# even numbers
 
 
 
-
-
+# PLOTTING ##### ##### ##### ##### ##### ##### ##### 
 
 # Let's plot this data.  You will need to 
 # load the library lattice, it is one of MANY packages
@@ -97,14 +122,26 @@ seq(2, 100, 2) 			# even numbers
 # http://cran.r-project.org/web/packages/
 library(lattice)
 
-
 # xyplot uses a formula for plotting with the form
-#		y ~ x1 + x2 | panel factor
+# y1 + y2 ~ x | factor1 + factor2
+xyplot(
+	x = Ta + Ts ~ Hour | as.factor(DOY),
+	data = dat,
+	type = "l" # Line type, try "b" and "p"
+)
+
+# Notice I treat DOY as a factor, this allows xyplot
+# to treat DOY as a unique factor instead of a numeric 
+# vector, you can try plotting DOY without, as.factor(),
+# the results are similar but the panels use a marker to
+# indicate progression along the numerical vector
+
 xyplot(
 	x = Ta + Ts ~ Hour | DOY,
 	data = dat,
 	type = "l" # Line type, try "b" and "p"
 )
+
 
 # What if we want to plot in a single panel?
 xyplot(
@@ -117,32 +154,32 @@ xyplot(
 xyplot(
 	x = Ta ~ Hour,
 	data = dat,
-	groups = DOY, 		# grouping factor
+	groups = DOY, # grouping factor
 	type = "l", 			
 	auto.key = list(TRUE, space = "right", points = F, lines = T)
 )
 
-
-
-
-
-
 # But really we want to see temperature as a single response across days,
 # to do so we need an time vector which includes both day and time
+# to assign the result to an object we use "<-" this is interchangeable with =
+# however <- is less ambiguous, in that it is clearly not an option within a function
 
-# Create Decimal Day (also known as julian day)
+# Create Decimal Day (also known as Julian day)
 DOY.dec <- dat$DOY + dat$Hour/24
 DOY.dec <- dat$DOY + dat$Hr/24 + dat$Min/1440 # Same as above
-length(DOY.dec) 	# this vector has 1582 values
-dim(dat) 	# gives the dimensions of the ?data.frame
-					# notice it has the same length as DOY.dec
+length(DOY.dec) # this vector has 1582 values
+dim(dat) # gives the dimensions of the data.frame
+
+# [1] 1582   10 
+# [row] length width
+# notice dat has the same length as DOY.dec
 					
-dat <- cbind(DOY.dec, dat) 
-# We have bound DOY.dec to dat, possible because they are of the same size
-# Alternatively create vector and tack on to the right of the data.frame
+dat <- cbind(DOY.dec, dat) #column bind function
+# We have bound DOY.dec to dat, possible because they are of the same length
+# Alternatively create the vector and tack on to the right of the data.frame
 # dat$DOY.dec <- dat$DOY + dat$Hour/24
 
-# now
+# now inspect
 dim(dat)
 head(dat)
 
@@ -160,15 +197,17 @@ xyplot(
 
 
 
+# Daily Max Min ##### ##### ##### ##### ##### ##### ##### 
 
 # Let's complete some more complicated tasks
 # calculate Daily Maximum Air Temperature
 # we will use the functions max and min
-# you can find other functions such as these by reading
-# the documentation
-?max
-?min
-?range
+# we are also using ?tapply, which allows us to apply
+# a function along an index, here we use dat$DOY 
+# which it interprets as 
+unique(dat$DOY) #[1] 200 201 202 203 204 205 206 207 208 209 210
+# and applies our function each day
+
 
 Ta.max <- tapply(X = dat$Ta, INDEX = dat$DOY, FUN = max)
 Ta.max  # vector of daily Temp max, with horizontal DOY index
@@ -184,23 +223,28 @@ Ta.min <- tapply(dat$Ta, dat$DOY, min)
 
 
 
+# Model ##### ##### ##### ##### ##### ##### ##### 
 
 # Next lets move to building and using the model
-
 # Fourier Temperature Series (Cambell & Norman p.23)
+# we will definite the equations as functions,
+# so that we can give simply give the function
+# our parameter and produce an output 
+
 # function gamma - accepts input time
-gamma <- function(time){
+fgamma <- function(time){
 	0.44 - 	0.46 * sin( { ( pi/12 ) * time } + 0.9 ) +
 		0.11 * sin( { 2 *  ( pi/12 ) * time } + 0.9 )
 }
 
-gamma(10) # [1] 0.59295
-gamma(0:10)
-
 # Fourier Series 
 fs <- function(time, Tn, Tx){
-	Tx * gamma(time) + Tn * (1 - gamma(time))
+	Tx * fgamma(time) + Tn * (1 - fgamma(time))
 }
+
+# using gamma
+fgamma(10) # [1] 0.59295
+fgamma(0:10)
 
 #Example 2.3 Page 23
 fs(10, Tn = 5, Tx = 23) # [1] 15.673
@@ -244,33 +288,23 @@ for( day in unique(dat$DOY) ){ print(day) }
 # ok, but running the for loop left day in the workspace, 
 # lets look
 ls()
-# [1] "dat"     "day"     "DOY.dec" "fs"      "gamma"   "i"       "Ta.max"  "Ta.min" 
+# [1] "dat"     "day"     "DOY.dec" "fs"      "fgamma"   "i"       "Ta.max"  "Ta.min" 
 
 # ah, there are a bunch of objects in the workspace, let's get rid of a couple
 rm(day, i, DOY.dec)
 
 # now
 ls()
-# [1] "dat"    "fs"     "gamma"  "Ta.max" "Ta.min"
+# [1] "dat"    "fs"     "fgamma"  "Ta.max" "Ta.min"
 
 # nice, much cleaner
 
 
 
 
-
-
-
-
-
-# Using everything we have learned up to this point, I am going to jump right in.
-
-# Usage of model ######## ######## ######## ########
-# 	Build an index for each time set of times
-#	apply each equation to match these times
-
-# Empty Vector of Not available Values
-dat$fTa <- rep(NA, length(dim(dat)[1]))
+# Applying the model ######## ######## ######## ########
+# Build an index for each time set of times
+# apply each equation to match these times
 
 Days <- unique(dat$DOY) #index of days
 Days 
@@ -278,7 +312,7 @@ Days
 # we only step through days
 Days[2:{length(Days) - 1}] # [1] 201 202 203 204 205 206 207 208 209
 
-# because we need Tx and Tn values from the first and last
+# because we need Ta.max and Ta.min values from the first and last
 # days to predict the temperature for the second and next to last day
 
 # also
@@ -292,29 +326,32 @@ Days[2:{length(Days) - 1}] # [1] 201 202 203 204 205 206 207 208 209
 Ta.max[as.character(201 - 1)]
 
 
-# Lets Write Our Model
+
+# Using everything we have learned up to this point, I am going to jump right in.
+
+# Empty Vector of NA "Not Available" Values
+dat$fTa <- rep(NA, length(dim(dat)[1]))
+Days <- unique(dat$DOY) #index of days
+
 for(day in Days[2:{length(Days) - 1}]){
+	
 	# Time index < or = 5 within the current day (dat$DOY %in% day)
 	time <- which(dat$Hour <= 5 & dat$DOY %in% day) 
 	
-	dat$fTa[time] <-	Ta.max[as.character(day - 1)] * gamma( dat$Hour[time] ) + 
-				Ta.min[as.character(day)] * { 1 - gamma( dat$Hour[time] ) }
-}
+	dat$fTa[time] <-	Ta.max[as.character(day - 1)] * fgamma( dat$Hour[time] ) + 
+				Ta.min[as.character(day)] * { 1 - fgamma( dat$Hour[time] ) }
 
-for(day in Days[2:{length(Days) - 1}]){
 	# Time index > 5  & Time < or = 14
 	time <- which(dat$Hour > 5 & dat$Hour <= 14 & dat$DOY %in% day)  
 
-	dat$fTa[time] <-	Ta.max[as.character(day)] * gamma( dat$Hour[time] ) + 
-				Ta.min[as.character(day)] * { 1 - gamma( dat$Hour[time] ) }
-}
-	
-for(day in Days[2:{length(Days) - 1}]){
+	dat$fTa[time] <-	Ta.max[as.character(day)] * fgamma( dat$Hour[time] ) + 
+				Ta.min[as.character(day)] * { 1 - fgamma( dat$Hour[time] ) }
+
 	# Time index > 14
 	time <- which(dat$Hour > 14 & dat$DOY %in% day) 
 
-	dat$fTa[time] <- 	Ta.max[as.character(day)] * gamma( dat$Hour[time] ) + 
-				Ta.min[as.character(day + 1)] * { 1 - gamma( dat$Hour[time] ) }
+	dat$fTa[time] <- 	Ta.max[as.character(day)] * fgamma( dat$Hour[time] ) + 
+				Ta.min[as.character(day + 1)] * { 1 - fgamma( dat$Hour[time] ) }
 }
 
 
@@ -324,25 +361,7 @@ for(day in Days[2:{length(Days) - 1}]){
 # There we go, Let's plot the data!
 
 xyplot(
-	x = fTa ~ DOY.dec,
-	data = dat,
-	type = "b",		# Line type, try "b" and "p"
-	auto.key = list(TRUE, points = F, lines = T),
-	main = "SoyFace Temperature Record",
-	xlab = "Julian Day"
-)
-
-xyplot(
 	x = fTa + Ta ~ DOY.dec,
-	data = dat,
-	type = "b",		# Line type, try "b" and "p"
-	auto.key = list(TRUE, points = F, lines = T),
-	main = "SoyFace Temperature Record",
-	xlab = "Julian Day"
-)
-
-xyplot(
-	x = fTa + Ta ~ Hour | as.factor(DOY),
 	data = dat,
 	type = "l",		# Line type, try "b" and "p"
 	auto.key = list(TRUE, points = F, lines = T),
@@ -350,9 +369,79 @@ xyplot(
 	xlab = "Julian Day"
 )
 
+
+# oh, interesting, we can immediately see that on some nights temperature
+# is underpredicted
+
+# on days in which the night time temperature falls much lower than that of the previous day, 
+# that the daily minimum occurs at the end of the day and not in the morning,
+# now this doesn't reflect the way the model was designed, where we use the daily
+# minimum and the previous day's max to determine the early morning hours in
+# which the daily morning minimum is reached.
+
+# As we have learned in class, temperature decreases over the course of the night,
+# not reaching it's minimum until sometime around 4 or 5 AM, just prior to sun rise
+# when solar radiation is applied to the surface.
+
+# ok, so let's fix this by calculating Ta.min only from those times before 10 am
+# this follows on the max/min weather station data collection mothod, where
+# temperatures are logged around 10 AM, recording the previous day's maximum
+# and the current morning minimum
+
+Ta.min.old <- tapply(dat$Ta, dat$DOY, min)
+Ta.min <- tapply(dat$Ta[dat$Hr < 10], dat$DOY[dat$Hr < 10], min)
+
+#and compare
+Ta.min.old
+Ta.min
+
+
+# Re run the model and plot
+dat$fTa <- rep(NA, length(dim(dat)[1]))
+Days <- unique(dat$DOY) #index of days
+
+for(day in Days[2:{length(Days) - 1}]){
+	
+	# Time index < or = 5 within the current day (dat$DOY %in% day)
+	time <- which(dat$Hour <= 5 & dat$DOY %in% day) 
+	
+	dat$fTa[time] <-	Ta.max[as.character(day - 1)] * fgamma( dat$Hour[time] ) + 
+				Ta.min[as.character(day)] * { 1 - fgamma( dat$Hour[time] ) }
+
+	# Time index > 5  & Time < or = 14
+	time <- which(dat$Hour > 5 & dat$Hour <= 14 & dat$DOY %in% day)  
+
+	dat$fTa[time] <-	Ta.max[as.character(day)] * fgamma( dat$Hour[time] ) + 
+				Ta.min[as.character(day)] * { 1 - fgamma( dat$Hour[time] ) }
+
+	# Time index > 14
+	time <- which(dat$Hour > 14 & dat$DOY %in% day) 
+
+	dat$fTa[time] <- 	Ta.max[as.character(day)] * fgamma( dat$Hour[time] ) + 
+				Ta.min[as.character(day + 1)] * { 1 - fgamma( dat$Hour[time] ) }
+}
+
+pTemp <- xyplot(
+	x = fTa + Ta ~ DOY.dec,
+	data = dat,
+	type = "l",		# Line type, try "b" and "p"
+	auto.key = list(TRUE, points = F, lines = T),
+	main = "SoyFace Temperature Record",
+	xlab = "Julian Day"
+)
+
+print(pTemp)
+
+# Much better!
+
+# save the plot as a pdf
+pdf(file = "predictedTemp.pdf")
+	print(pTemp)
+dev.off()
+
 # Hey, now you are a  useR! R is a fantastic tool
 # for both investigating datasets, and for plotting and modeling data
 # again feel free to email me or instant message on gchat if you get stuck
 
-# I have re-written the model to be more concise and take inputs in any time incriment
+# I have re-written the model to be more concise 
 # take a look at run.model.R for useage, and model.Functions.R for the source
